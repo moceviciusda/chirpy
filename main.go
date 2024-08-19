2093d7c6f8b440ec3abfd279689b47e1e1a60f20
moceviciusda <moceviciusdarius@gmail.com>
2024-08-19 13:47:02 +0300

    handle a fouple of routes

--- main.go
+++ main.go
@@ -7,9 +7,19 @@ import (
 func main() {
 	const port = "8080"
 
+	handler := http.NewServeMux()
+
+	handler.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+
+	handler.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(200)
+		w.Write([]byte("OK"))
+	})
+
 	server := &http.Server{
 		Addr:    "localhost:" + port,
-		Handler: http.NewServeMux(),
+		Handler: handler,
 	}
 
 	server.ListenAndServe()
